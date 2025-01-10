b4653a72073a24cd9292fc787e892e23592df77f
TanaySoni21 <160638291+TanaySoni21@users.noreply.github.com>
2025-01-10 22:55:18 +0530

    starting storage management

---
+++ Assets/Assets/Scripts/StorageManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class StorageManager : MonoBehaviour
+{
+    private StorageManager Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+}
